706cf0677326f022695f3bca6e7ace73a6159706
mmx369 <maksim@klishevich.com>
2021-09-08 23:41:35 +0300

    ffff

--- cypress/integration/home.spec.js
+++
@@ -1,9 +0,0 @@
-context('Home Page', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
-  it('should find our start message', () => {
-    cy.get('div').contains('Нумизматика')
-  })
-})
--- cypress/integration/test.spec.js
+++
@@ -1,8 +0,0 @@
-const types = ['Paper Money', 'Coins', 'Other']
-
-context('Index Page', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-  it('Check our types', () => {})
-})
